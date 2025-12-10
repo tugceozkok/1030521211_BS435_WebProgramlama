cf3d120dc95f56c95653e52bff758021312383d4
Tuğçe Özkök <1030521211@erciyes.edu.tr>
2025-12-11 00:21:32 +0300

    .

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const questions = [
+    {
+      id: 1,
+      images: [
+        { id: 'img1', src: '/images/real1.jpg', isAi: false },
+        { id: 'img2', src: '/images/ai1.jpg', isAi: true }, 
+        { id: 'img3', src: '/images/real2.jpg', isAi: false },
+      ],
+      hint: "Yüz simetrisine ve göz bebeklerine dikkat et.",
+      category: "Portre"
+    },
+];
